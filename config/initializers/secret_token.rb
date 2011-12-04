87f65460ea173b5f4d378037089364a18c6a9210
will usher <will@williamsmash.com>
2011-12-03 18:55:32 -0800

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pointup::Application.config.secret_token = 'dd99cc8356a23ad4ea72c0bc95effdb3b51affe652d2d97b249a734e1442be10a61dea04303d78f0f54eed4f0fcd675173b629de6203009d0ed2ec3dc2bcc3ed'
